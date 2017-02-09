8b8f97b3c85b708f3a6417b6058b5a328d348285
somawork89 <somawork89@gmail.com>
2017-02-09 15:28:08 -0800

    commit all

---
+++ ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product {ID=1,Name="Wildcraft",Category="backpack",Price=20 },
+            new Product {ID=2,Name="Michelkores",Category="purse",Price=80 }
+        };
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+    }
+}
